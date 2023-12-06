b75a7fe847a32d2c2e3cd7903fde5c78ce28a8aa
Darius A. Görgen <info@dariusgoergen.com>
2023-12-06 16:42:29 +0100

    changed inst location name

--- data-raw/carbon.R
+++ data-raw/carbon.R
@@ -5,30 +5,33 @@ library(sf)
 aoi <- st_read(system.file("shape/nc.shp", package="sf")) |>
   st_transform("EPSG:4326") |>
   st_cast("POLYGON") |>
-  init_portfolio(years = c(2000:2020), outdir = "../data/", add_resources = FALSE) |>
+  init_portfolio(years = c(2000:2020), outdir = "../data/") |>
   get_resources(c("irr_carbon", "vul_carbon", "man_carbon"))
 
-tindex_irr <- attr(aoi, "resources")["irr_carbon"] |> st_read()
+tindex_irr <- attr(aoi, "resources")[["irr_carbon"]]
 irr_carbon <- rast(tindex_irr[["location"]])
 irr_carbon <- crop(irr_carbon, aoi)
 
-outdir <- "inst/irr_carbon"
+outdir <- "inst/resources/irr_carbon"
 dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
 writeRaster(irr_carbon, file.path(outdir, basename(tindex_irr[["location"]])))
+file.create(file.path(outdir, paste0("Irrecoverable_Carbon_", c(2010, 2018), ".zip")))
 
-tindex_vul <- attr(aoi, "resources")["vul_carbon"] |> st_read()
+tindex_vul <- attr(aoi, "resources")[["vul_carbon"]]
 vul_carbon <- rast(tindex_vul[["location"]])
 vul_carbon <- crop(vul_carbon, aoi)
 
-outdir <- "inst/vul_carbon"
+outdir <- "inst/resources/vul_carbon"
 dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
 writeRaster(vul_carbon, file.path(outdir, basename(tindex_vul[["location"]])))
+file.create(file.path(outdir, paste0("Vulnerable_Carbon_", c(2010, 2018), ".zip")))
 
-tindex_man <- attr(aoi, "resources")["man_carbon"] |> st_read()
+tindex_man <- attr(aoi, "resources")[["man_carbon"]]
 man_carbon <- rast(tindex_man[["location"]])
 man_carbon <- crop(man_carbon, aoi)
 
-outdir <- "inst/man_carbon"
+outdir <- "inst/resources/man_carbon"
 dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
-writeRaster(man_carbon, file.path(outdir, basename(tindex_vul[["location"]])))
+writeRaster(man_carbon, file.path(outdir, basename(tindex_man[["location"]])))
+file.create(file.path(outdir, paste0("Manageable_Carbon_", c(2010, 2018), ".zip")))
 

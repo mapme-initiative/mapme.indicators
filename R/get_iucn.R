19006acbe5d24bde8abaaeba7ae33f61facff560
Darius A. Görgen <info@dariusgoergen.com>
2024-04-09 13:19:33 +0200

    Update IUCN to new UI

--- R/get_iucn.R
+++ R/get_iucn.R
@@ -12,235 +12,162 @@
 #' Note, that we are simplifying the species ranges polygons for better computational
 #' efficiency.
 #'
-#' @name iucn_mammals
-#' @docType data
+#' @name species_ranges
+#' @param path A character vector to the respective species range file that
+#'   has to be downloaded manually.
 #' @keywords resource
-#' @format A global vector data set indicating species ranges
+#' @returns A function that returns a character vector of file paths.
 #' @references IUCN (2024). The IUCN Red List of Threatened Species. https://www.iucnredlist.org.
-#' https://doi.org/10.1038/s41597-022-01284-8
+#' \doi{https://doi.org/10.1038/s41597-022-01284-8 }
 #' @source \url{https://www.iucnredlist.org/resources/spatial-data-download}
-NULL
+#' @export
+get_iucn_mammals <- function(path = NULL) {
 
-
-#' @noRd
-#' @include zzz.R
-.get_iucn_mammals <- function(
-    x,
-    path = NULL,
-    rundir = tempdir(),
-    verbose = TRUE) {
-
-  if(is.null(path)){
-    stop("'path' cannot be NULL.")
-  }
-
-  if(basename(path) != "MAMMALS.zip"){
+  if(is.null(path) || !file.exists(path) || basename(path) != "MAMMALS.zip") {
     stop("Expecting path to point towards a file called: 'MAMMALS.zip'.")
   }
 
-  dst <- file.path(rundir, gsub("zip", "fgb", basename(path)))
-  src <- paste0("/vsizip/", path)
-  if(!file.exists(dst)) .prep_iucn(src, dst, verbose = verbose)
-  dst
+  function(
+    x,
+    name = "iucn_mammals",
+    type = "vector",
+    outdir = mapme_options()[["outdir"]],
+    verbose = mapme_options()[["verbose"]],
+    testing = mapme_options()[["testing"]]) {
+
+    dst <- file.path(outdir, gsub("zip", "fgb", basename(path)))
+    src <- paste0("/vsizip/", path)
+    if(!file.exists(dst)) .prep_iucn(src, dst, verbose = verbose)
+    dst
+  }
 }
 
-
-.register(list(
+register_resource(
   name = "iucn_mammals",
-  type = "vector",
+  description = "Potential species ranges for mammals.",
+  licence = "https://www.iucnredlist.org/terms/terms-of-use",
   source = "https://www.iucnredlist.org/resources/spatial-data-download",
-  fun = .get_iucn_mammals,
-  arguments = list(
-    path = NULL
-  )),
-  "resource")
-
-#' IUCN Red List of Threatened Species - Reptiles
-#'
-#' This resource is part of the spatial data set Red List of Threatened Species
-#' released by IUCN. The data are made available including taxonomic information,
-#' distribution status, IUCN Red List Category, sources and other details. This
-#' resource is free to use under a non-commercial licence. For commercial uses,
-#' a request has to be sent to Integrated Biodiversity Assessment Tool (IBAT).
-#'
-#' To use this data in mapme workflows, you will have to manually download the
-#' global data set and point towards the file path on your local machine.
-#'
-#' Note, that we are simplifying the species ranges polygons for better computational
-#' efficiency.
-#'
-#' @name iucn_reptiles
-#' @docType data
-#' @keywords resource
-#' @format A global vector data set indicating species ranges
-#' @references IUCN (2024). The IUCN Red List of Threatened Species. https://www.iucnredlist.org.
-#' https://doi.org/10.1038/s41597-022-01284-8
-#' @source \url{https://www.iucnredlist.org/resources/spatial-data-download}
-NULL
+  type = "vector"
+)
 
 
-#' @noRd
-#' @include zzz.R
-.get_iucn_reptiles <- function(
-    x,
-    path = NULL,
-    rundir = tempdir(),
-    verbose = TRUE) {
+#' @name species_ranges
+#' @export
+get_iucn_reptiles <- function(path = NULL) {
 
-  if(is.null(path)){
-    stop("'path' cannot be NULL.")
-  }
-
-  if(basename(path) != "REPTILES.zip"){
+  if(is.null(path) || !file.exists(path) || basename(path) != "REPTILES.zip") {
     stop("Expecting path to point towards a file called: 'REPTILES.zip'.")
   }
 
-  dst <-  file.path(rundir, gsub("zip", "gpkg", basename(path)))
-  src <- paste0("/vsizip/", path)
-  layers <- st_layers(src)[["name"]]
-  if(length(layers) != 2) {
-    stop("Expected REPTILES.zip to have two layers.")
+  function(
+    x,
+    name = "iucn_reptiles",
+    type = "vector",
+    outdir = mapme_options()[["outdir"]],
+    verbose = mapme_options()[["verbose"]],
+    testing = mapme_options()[["testing"]]) {
+
+    dst <-  file.path(outdir, gsub("zip", "gpkg", basename(path)))
+    src <- paste0("/vsizip/", path)
+    layers <- st_layers(src)[["name"]]
+    if(length(layers) != 2) {
+      stop("Expected REPTILES.zip to have two layers.")
+    }
+    if(!file.exists(dst)) .prep_iucn(src, dst, layers, verbose = verbose)
+    dst
   }
-  if(!file.exists(dst)) .prep_iucn(src, dst, layers, verbose = verbose)
-  dst
 }
 
 
-.register(list(
+register_resource(
   name = "iucn_reptiles",
-  type = "vector",
+  description = "Potential species ranges for reptiles.",
+  licence = "https://www.iucnredlist.org/terms/terms-of-use",
   source = "https://www.iucnredlist.org/resources/spatial-data-download",
-  fun = .get_iucn_reptiles,
-  arguments = list(
-    path = NULL
-  )),
-  "resource")
+  type = "vector"
+)
 
-#' IUCN Red List of Threatened Species - Amphibians
-#'
-#' This resource is part of the spatial data set Red List of Threatened Species
-#' released by IUCN. The data are made available including taxonomic information,
-#' distribution status, IUCN Red List Category, sources and other details. This
-#' resource is free to use under a non-commercial licence. For commercial uses,
-#' a request has to be sent to Integrated Biodiversity Assessment Tool (IBAT).
-#'
-#' To use this data in mapme workflows, you will have to manually download the
-#' global data set and point towards the file path on your local machine.
-#'
-#' Note, that we are simplifying the species ranges polygons for better computational
-#' efficiency.
-#'
-#' @name iucn_amphibians
-#' @docType data
-#' @keywords resource
-#' @format A global vector data set indicating species ranges
-#' @references IUCN (2024). The IUCN Red List of Threatened Species. https://www.iucnredlist.org.
-#' https://doi.org/10.1038/s41597-022-01284-8
-#' @source \url{https://www.iucnredlist.org/resources/spatial-data-download}
-NULL
+#' @name species_ranges
+#' @export
+get_iucn_amphibians <- function(path = NULL) {
 
-
-#' @noRd
-#' @include zzz.R
-.get_iucn_amphibians <- function(
-    x,
-    path = NULL,
-    rundir = tempdir(),
-    verbose = TRUE) {
-
-  if(is.null(path)){
-    stop("'path' cannot be NULL.")
-  }
-
-  if(basename(path) != "AMPHIBIANS.zip"){
+  if(is.null(path) || !file.exists(path) || basename(path) != "AMPHIBIANS.zip") {
     stop("Expecting path to point towards a file called: 'AMPHIBIANS.zip'.")
   }
 
-  dst <- file.path(rundir, gsub("zip", "fgb", basename(path)))
-  src <- paste0("/vsizip/", path)
-  layers <- st_layers(src)[["name"]]
-  if(length(layers) != 2) {
-    stop("Expected AMPHIBIANS.zip to have two layers.")
+  function(
+    x,
+    name = "iucn_amphibians",
+    type = "vector",
+    outdir = mapme_options()[["outdir"]],
+    verbose = mapme_options()[["verbose"]],
+    testing = mapme_options()[["testing"]]) {
+
+    dst <- file.path(outdir, gsub("zip", "fgb", basename(path)))
+    src <- paste0("/vsizip/", path)
+    layers <- st_layers(src)[["name"]]
+    if(length(layers) != 2) {
+      stop("Expected AMPHIBIANS.zip to have two layers.")
+    }
+    if(!file.exists(dst)) .prep_iucn(src, dst, verbose = verbose)
+    dst
   }
-  if(!file.exists(dst)) .prep_iucn(src, dst, verbose = verbose)
-  dst
 }
 
 
-.register(list(
+register_resource(
   name = "iucn_amphibians",
-  type = "vector",
+  description = "Potential species ranges for amphibians.",
+  licence = "https://www.iucnredlist.org/terms/terms-of-use",
   source = "https://www.iucnredlist.org/resources/spatial-data-download",
-  fun = .get_iucn_amphibians,
-  arguments = list(
-    path = NULL
-  )),
-  "resource")
+  type = "vector"
+)
 
-
-#' BirdLife International Species Ranges
-#'
-#' This resource is part of the spatial data set of bird ranges
+#' The birdlife resource is part of the spatial data set of bird ranges
 #' released by BirdLife International. This resource is free to use under a
 #' non-commercial licence. For commercial uses, a request has to be sent to
 #' BirdLife International.
 #'
-#' To use this data in mapme workflows, you will have to manually download the
-#' global data set and point towards the file path on your local machine.
-#'
-#' Note, that we are simplifying the species ranges polygons for better computational
-#' efficiency.
-#'
-#' @name birdlife
-#' @docType data
-#' @keywords resource
-#' @format A global vector data set indicating species ranges
+#' @name species_ranges
 #' @references BirdLife International and Handbook of the Birds of the World (2022).
 #'   Bird species distribution maps of the world. Version 2022.2. Available at
 #'   http://datazone.birdlife.org/species/requestdis
-#' @source \url{http://datazone.birdlife.org/species/requestdis}
-NULL
+#' @export
+get_birdlife <- function(path = NULL) {
 
-
-#' @noRd
-#' @include zzz.R
-.get_birdlife <- function(
-    x,
-    path = NULL,
-    rundir = tempdir(),
-    verbose = TRUE) {
-
-  if(is.null(path)){
-    stop("'path' cannot be NULL.")
-  }
-
-  if(basename(path) != "BOTW.gdb"){
+  if(is.null(path) || !file.exists(path) || basename(path) != "BOTW.gdb") {
     stop("Expecting path to point towards a file called: 'BOTW.gdb'.")
   }
 
-  layers <- st_layers(path)
-  polys_layer <- grep("Species", layers$name, value = TRUE)
-  meta_layer <- grep("Species", layers$name, value = TRUE, invert = TRUE)
-  if (length(polys_layer) != 1 || length(meta_layer) != 1) {
-    stop("Expected 'birdlife' to have exactly two layers. Please raise an issue.")
-  }
-  sql <- sprintf("select * from %s inner join %s on %s.sisid=%s.sisid;",
-                 polys_layer, meta_layer, polys_layer, meta_layer)
-  dst <- file.path(rundir, gsub("gdb", "fgb", basename(path)))
-  if(!file.exists(dst)) .prep_iucn(path, dst, sql, verbose)
-  dst
+  function(
+    x,
+    name = "birdlife",
+    type = "vector",
+    outdir = mapme_options()[["outdir"]],
+    verbose = mapme_options()[["verbose"]],
+    testing = mapme_options()[["testing"]]) {
 
+    layers <- st_layers(path)
+    polys_layer <- grep("Species", layers$name, value = TRUE)
+    meta_layer <- grep("Species", layers$name, value = TRUE, invert = TRUE)
+    if (length(polys_layer) != 1 || length(meta_layer) != 1) {
+      stop("Expected 'birdlife' to have exactly two layers. Please raise an issue.")
+    }
+    sql <- sprintf("select * from %s inner join %s on %s.sisid=%s.sisid;",
+                   polys_layer, meta_layer, polys_layer, meta_layer)
+    dst <- file.path(outdir, gsub("gdb", "fgb", basename(path)))
+    if(!file.exists(dst)) .prep_iucn(path, dst, sql, verbose)
+    dst
+  }
 }
 
-.register(list(
+register_resource(
   name = "birdlife",
-  type = "vector",
+  description = "Potential species ranges for birds.",
+  licence = "https://datazone.birdlife.org/info/dataterms",
   source = "http://datazone.birdlife.org/species/requestdis",
-  fun = .get_birdlife,
-  arguments = list(
-    path = NULL
-  )),
-  "resource")
+  type = "vector"
+)
 
 
 .prep_iucn <- function(src, dst, layers = NULL, sql = NULL, verbose = TRUE) {

83a15510c05748026734975feb89d14e1a71ee8f
Darius A. Görgen <info@dariusgoergen.com>
2024-05-21 12:19:15 +0200

    Updates IPBES resource and indicator

--- R/calc_ipbes.R
+++ R/calc_ipbes.R
@@ -11,32 +11,36 @@
 #' @returns A function that returns a tibble with columns for the class name,
 #'   its absolute and relative coverage within an asset.
 #' @export
-calc_ipbes_biome <- function() {
+calc_ipbes_biomes <- function() {
 
   function(
     x,
-    ipbes_biome,
-    name = "ipbes_biome_stats",
+    ipbes_biomes,
+    name = "ipbes_biomes",
     mode = "portfolio",
+    aggregation = "sum",
     verbose = mapme_options()[["verbose"]]) {
 
-    if (is.null(ipbes_biome)) return(NA)
+    if (is.null(ipbes_biomes)) return(NA)
+    if (st_crs(x) != st_crs(ipbes_biomes)) {
+      x <- st_transform(x, st_crs(ipbes_biomes))
+    }
 
     results <- lapply(1:nrow(x), function(i) {
-      exactextractr::exact_extract(ipbes_biome, x[i, ], fun = function(data, class_df){
+      exactextractr::exact_extract(ipbes_biomes, x[i, ], fun = function(data, class_df){
 
         data[["coverage_area"]] <- data[["coverage_area"]] / 10000
-        total_area <- sum(data[["coverage_area"]])
         class_area <- by(data[["coverage_area"]], data[["value"]], sum)
         result <- tibble::tibble(
-          class = as.numeric(names(class_area)),
-          area = as.numeric(class_area),
-          percentage = as.numeric(class_area) / total_area * 100
-        )
-        result[["class"]] <- class_df[["name"]][result[["class"]]]
-        result
+          variable = as.numeric(names(class_area)),
+          unit = "ha",
+          value = as.numeric(class_area))
+        result[["variable"]] <- class_df[["name"]][result[["variable"]]]
+        result[["variable"]] <- gsub(" ", "_", result[["variable"]])
+        result[["datetime"]] <- as.Date("2019-01-01")
+        result[ ,c("datetime", "variable", "unit", "value")]
 
-      }, class_df = ipbes_biome_classes, coverage_area = TRUE, summarize_df = TRUE)
+      }, class_df = .ipbes_biome_classes, coverage_area = TRUE, summarize_df = TRUE)
     })
 
     results
@@ -44,69 +48,7 @@ calc_ipbes_biome <- function() {
 }
 
 register_indicator(
-  name = "ipbes_biome_stats",
+  name = "ipbes_biomes",
   description = "Area distibution of IBPES biomes within a polygon.",
-  resources = "ipbes_biome"
-)
-
-#' Calculate areal statistics for IBPES Anthromes
-#'
-#' This indicator calculates the areal distribution of the two different anthrome
-#' classes within an asset based on the IBPES anthrome dataset. You can select
-#' to only include areal statistics for built-up areas, cultivated areas, or both.
-#'
-#' The required resources for this indicator are:
-#'  - [ipbes]
-#'
-#' @name ipbes_anthrome_stats
-#' @param anthrome One of "urban_areas", "cultivated_areas", "both". Determines
-#'     for which data layer the areal statistics are calculated.
-#' @keywords indicator
-#' @returns A function that returns a tibble with columns for the respective
-#'  anthrome name, the affected area, the percentage of the affected area in
-#'  relation to the total area, and the total area (all expressed in ha).
-#' @export
-calc_ipbes_anthrome <- function(anthrome = c("urban_areas", "cultivated_areas", "both")) {
-
-  anthrome <- match.arg(anthrome)
-
-  function(
-    x,
-    ipbes_anthrome,
-    name = "ipbes_anthrome_stats",
-    mode = "portfolio",
-    verbose = mapme_options()[["verbose"]]) {
-
-    if (is.null(ipbes_anthrome)) return(NA)
-    if (anthrome == "both") anthrome <- c("urban_areas", "cultivated_areas")
-    names(ipbes_anthrome) <- c("urban_areas", "cultivated_areas")
-    ipbes_anthrome <- ipbes_anthrome[[anthrome]]
-
-    results <- lapply(1:nrow(x), function(i) {
-      exactextractr::exact_extract(ipbes_anthrome, x[i, ], fun = function(data, anthrome){
-
-        data[["coverage_area"]] <- data[["coverage_area"]] / 10000
-        result <- lapply(anthrome, function(a){
-          affected <- sum((data[[a]]/100) * data[["coverage_area"]])
-          total = sum(data[["coverage_area"]])
-          percent = affected / total * 100
-          tibble::tibble(
-            anthrome = a,
-            affected = affected,
-            percentage = percent,
-            total = total
-          )
-        })
-        do.call(rbind, result)
-
-      }, anthrome = anthrome, coverage_area = TRUE, summarize_df = TRUE)
-    })
-    results
-  }
-}
-
-register_indicator(
-  name = "ipbes_anthrome_stats",
-  description = "Area distibution of IBPES anthrome classes within a polygon.",
   resources = "ipbes_biome"
 )
--- data-raw/ipbes.R
+++ data-raw/ipbes.R
@@ -1,26 +1,19 @@
-library(LLFindicators)
+library(mapme.indicators)
 library(terra)
 library(sf)
 
-aoi <- st_read(system.file("shape/nc.shp", package="sf")) %>%
-  st_transform("EPSG:4326") %>%
-  st_cast("POLYGON") %>%
-  init_portfolio(years = c(2000:2020), outdir = "../data/") %>%
-  get_resources(c("ipbes_anthrome", "ipbes_biome"))
+x <- st_read(system.file("shape/nc.shp", package="sf"))
+x <- x[5, ]
+x <- st_transform(x, "EPSG:4326")
 
-tindex_biome <- st_read(attr(aoi, "resources")["ipbes_biome"])
-biome <- rast(tindex_biome[["location"]])
-biome <- crop(biome, aoi)
+outdir <- tempfile()
+dir.create(outdir)
+mapme_options(outdir = outdir)
 
-outdir <- "inst/ipbes_biome"
-dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
-writeRaster(biome, file.path(outdir, basename(tindex_biome[["location"]])))
+get_resources(x, get_ipbes_biomes())
+biome <- prep_resources(x)$ipbes_biomes
 
-tindex_anthrome <- st_read(attr(aoi, "resources")["ipbes_anthrome"])
-anthrome <- rast(tindex_anthrome[["location"]])
-anthrome <- crop(anthrome, aoi)
-
-outdir <- "inst/ipbes_anthrome"
-dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
-writeRaster(anthrome, file.path(outdir, basename(tindex_anthrome[["location"]])))
-
+writeRaster(biome,
+            filename = file.path("inst/resources/ipbes_biomes/", paste0(names(biome), ".tif")),
+            datatype = "INT1U", gdal = c("COMPRESS=LZW"),
+            overwrite = TRUE)

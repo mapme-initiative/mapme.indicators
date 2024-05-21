f4112aab73ff70d7c454f31b902845c3d14578da
Darius A. Görgen <info@dariusgoergen.com>
2024-05-21 12:17:30 +0200

    Uses IUCN SR rasters instead of range data

--- R/calc_iucn.R
+++ R/calc_iucn.R
@@ -1,131 +1,63 @@
-#' Species richness
+#' Species richness based on IUCN raster data
 #'
 #' Species richness counts the number of potential species intersecting with a
-#' polygon grouped by the IUCN threat categorization. For each animal group,
-#' there is an indicator function which requires the availablity of the
-#' respctive resource.
+#' polygon grouped by the IUCN threat categorization. Note, that
+#' this indicator function requires the manual download of the respective
+#' raster files.
+#'
+#' The specific meaning of the species richness indicator depends on the
+#' supplied raster file.
 #'
 #' The required resources for this indicator are:
-#'  - see the respective [species_ranges]
+#'  - [iucn]
 #'
-#' @name sr_mammals
+#' @name species_richness
+#' @param engine The preferred processing functions from either one of "zonal",
+#'   "extract" or "exactextract" as character.
+#' @param stats Function to be applied to compute statistics for polygons either
+#'   one or multiple inputs as character. Supported statistics are: "mean",
+#'   "median", "sd", "min", "max", "sum" "var".
+#' @param variable A character used as the variable column output. Defaults to
+#'   `species_richness` but can be set to reflect the manually specified raster
+#'   file.
 #' @keywords indicator
-#' @returns A function that returns a tibble with columns for the code and the
-#'   full-text threat category as well as the count of the number of respective species.
+#' @returns A function that returns a tibble with statistics on the species
+#'   richness.
 #' @export
-calc_sr_mammals <- function() {
+calc_species_richness <- function(engine = "extract", stats = "mean",
+                                  variable = "species_richness") {
+  engine <- check_engine(engine)
+  stats <- check_stats(stats)
+  stopifnot(is.character(variable) || length(variable) == 1)
+
   function(
     x,
-    iucn_mammals,
-    name = "sr_mammals",
+    iucn,
+    name = "species_richness",
     mode = "asset",
+    aggregation = "stat",
     verbose = mapme_options()[["verbose"]]) {
 
-    iucn_mammals <- iucn_mammals[[1]]
-    if(is.null(iucn_mammals)) return(NA)
-    .calc_sr(table(iucn_mammals[["category"]]), "sr_mammals")
+    x <- st_transform(x, st_crs(iucn))
+
+    result <- select_engine(
+      x = x,
+      raster = iucn,
+      stats = stats,
+      engine = engine,
+      name = variable,
+      mode = "asset"
+    )
+    datetime <- strsplit(names(iucn), "_")[[1]]
+    result[["datetime"]] <-  as.Date(paste0(rev(datetime)[1], "-01-01"))
+    result <- tidyr::pivot_longer(result, cols = -datetime, names_to = "variable")
+    result[["unit"]] <- "count"
+    result[ ,c("datetime", "variable", "unit", "value")]
   }
 }
 
 register_indicator(
-  name = "sr_mammals",
-  description = "Species richness count for mammals by threat category.",
-  resources = "iucn_mammals"
+  name = "species_richness",
+  description = "Species richness statistics based on user-specified raster files.",
+  resources = "iucn"
 )
-
-#' @name sr_reptiles
-#' @keywords indicator
-#' @inherit sr_mammals title description details return
-#' @export
-calc_sr_reptiles <- function() {
-  function(
-    x,
-    iucn_reptiles,
-    name = "sr_reptiles",
-    mode = "asset",
-    verbose = mapme_options()[["verbose"]]) {
-
-    iucn_reptiles <- iucn_reptiles[[1]]
-    if(is.null(iucn_reptiles)) return(NA)
-    .calc_sr(table(iucn_reptiles[["category"]]), "sr_reptiles")
-  }
-}
-
-register_indicator(
-  name = "sr_reptiles",
-  description = "Species richness count for reptiles by threat category.",
-  resources = "iucn_reptiles"
-)
-
-#' @name sr_amphibians
-#' @keywords indicator
-#' @inherit sr_mammals title description details return
-#' @export
-calc_sr_amphibians <- function(){
-  function(
-    x,
-    iucn_amphibians,
-    name = "sr_amphibians",
-    mode = "asset",
-    verbose = mapme_options()[["verbose"]]) {
-
-    iucn_amphibians <- iucn_amphibians[[1]]
-    if(is.null(iucn_amphibians)) return(NA)
-    .calc_sr(table(iucn_amphibians[["category"]]), "sr_amphibians")
-  }
-}
-
-register_indicator(
-  name = "sr_amphibians",
-  description = "Species richness count for amphibians by threat category.",
-  resources = "iucn_amphibians"
-)
-
-
-#' @name sr_birds
-#' @keywords indicator
-#' @inherit sr_mammals title description details return
-#' @export
-calc_sr_birds <- function() {
-  function(
-    x,
-    birdlife,
-    name = "sr_birds",
-    mode = "asset",
-    verbose = mapme_options()[["verbose"]]) {
-
-    birdlife <- birdlife[[1]]
-    if(is.null(birdlife)) return(NA)
-    col_index <- grep("IUCN_Red_List_Category", names(birdlife))
-    names(birdlife)[col_index] <- "category"
-    .calc_sr(table(birdlife[["category"]]), "sr_birds")
-  }
-}
-
-register_indicator(
-  name = "sr_birds",
-  description = "Species richness count for birds by threat category.",
-  resources = "birdlife"
-)
-
-
-.calc_sr <- function(cats, var = NULL) {
-  out <- tibble::tibble(
-    code = c("EX", "EW", "CR", "EN", "VU", "NT", "LC", "DD", "NE"),
-    category = c("Extinct",
-                 "Extinct in the wild",
-                 "Critically endangered",
-                 "Endangered",
-                 "Vulnerable",
-                 "Near threatened",
-                 "Least concern",
-                 "Data deficient",
-                 "Not evaluated"),
-    count = rep(0, 9)
-  )
-
-  index <- match(names(cats), out[["code"]])
-  out[["count"]][index] <- cats
-  out["variable"] <- var
-  out[ ,c("variable", "code", "category", "count")]
-}

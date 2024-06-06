2df2341330c3ac5eb4053287da1dfd461fae8c1e
Darius A. Görgen <info@dariusgoergen.com>
2024-06-06 11:00:22 +0200

    remove snapshot tests

--- tests/testthat/test-calc_ipbes.R
+++ tests/testthat/test-calc_ipbes.R
@@ -8,7 +8,5 @@ test_that("calc_ipbes_biomes works", {
   ind <- calc_indicators(x, calc_ipbes_biomes())
   expect_true("ipbes_biomes" %in% names(ind))
   expect_equal(class(ind[["ipbes_biomes"]]), "list")
-  expect_equal(ind[["ipbes_biomes"]][[1]][["value"]],
-               c(145244.758, 7061.708))
 })
 
